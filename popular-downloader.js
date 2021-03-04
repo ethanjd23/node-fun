ff9384f707e853370753dc4e0084c74db2d3c2d2
ethanjd23 <ethanjd23@gmail.com>
2021-03-04 14:25:37 -0600

    advanced added, but not working

---
+++ popular-downloader.js
@@ -0,0 +1,30 @@
+const request = require("request");
+const path = require("path");
+const fs = require("fs");
+
+let dataPath = path.join(__dirname, "./downloads/");
+
+request("https://reddit.com/r/popular.json", (err, res, body) => {
+  if (err) {
+    console.log(err);
+  }
+
+  JSON.parse(body).data.children.forEach((item) => {
+    if (
+      path.extname(item.data.url) === ".jpg" ||
+      path.extname(item.data.url) === ".gifv" ||
+      path.extname(item.data.url) === ".png" 
+    ) { 
+        fs.writeFile(dataPath, item.data.url, (err) => {
+                if (err) {
+                  console.log(err);
+                }
+              });   
+    }
+  });
+//   fs.writeFile(dataPath, JSON.stringify(redditArray), (err) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//   });
+});
